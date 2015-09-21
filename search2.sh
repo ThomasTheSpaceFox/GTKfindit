67f7d2d96aebefc626467eede813a33faadf6d11
Thomas Leathers <teleathers22@gmail.com>
2015-09-21 09:32:56 +0800

    reworked the search seript to just use find.
    i reworked the search script to just use find.
    this seems to speed up the search.

--- search2.sh
+++ search2.sh
@@ -6,28 +6,24 @@ cd $WHEREAMI
 FINDIT=$(cat find.it.txt)
 DIRECT=$(cat find.direct.txt)
 echo "GTKfindit -find what you need." > results.txt
+echo "______________________________________" >> results.txt
 echo "Search:$FINDIT" >> results.txt
 echo "directory:$DIRECT" >> results.txt
+echo "______________________________________" >> results.txt
 echo "searching, please wait..."
 echo "start time: $(date)" >>  results.txt
-echo "0" > counter.txt
-echo "0" > counter2.txt
+echo "______________________________________" >> results.txt
 echo "" >> results.txt
 
-find $DIRECT -type d > SUBDIRLST.txt
-BULB=$(cat SUBDIRLST.txt)
-shopt -s nullglob
-for f in $BULB
-do
-$WHEREAMI/dirlook.sh "$f/$FINDIT"
-done
+find "$DIRECT" -name "$FINDIT" >> results.txt
+
 
 echo "" >> results.txt
 echo "" >> results.txt
+echo "______________________________________" >> results.txt
 echo "end of search results for $FINDIT" >> results.txt
 echo "end time: $(date)" >>  results.txt
 echo "under $DIRECT and sub-directories" >> results.txt
-echo "GTKfindit found $(cat counter.txt) results" >> results.txt
-echo -e "in $(cat counter2.txt) directories." >> results.txt
 echo "" >> results.txt
+echo "______________________________________" >> results.txt
 echo "GTKfindit -find what you need. " >> results.txt
